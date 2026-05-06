49a0ee4cb9f518c8694bc06c5297c0f08bfbc4fa
Victor Omos <victor@gridlockaz.com>
2026-05-06 12:21:56 +0100

    Automated Commit

--- src/components/PoliciesTable.tsx
+++ src/components/PoliciesTable.tsx
@@ -143,7 +143,7 @@ export default function PoliciesTable() {
 
     try {
       const res = await fetch(
-        `${process.env.NEXT_PUBLIC_API_URL}/ins_policy/list?userid=${currentUsername}&policy=`
+        `${process.env.NEXT_PUBLIC_API_URL}/ins_policy/list/${currentUsername}`
       );
       if (!res.ok) throw new Error(`HTTP ${res.status}`);
       const data = await res.json();
