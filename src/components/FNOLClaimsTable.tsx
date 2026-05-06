5b12cef88a3bac674ee9044267f023e784ac7cb8
Victor Omos <victor@gridlockaz.com>
2026-05-06 15:14:01 +0100

    Automated Commit

--- src/components/FNOLClaimsTable.tsx
+++ src/components/FNOLClaimsTable.tsx
@@ -135,9 +135,9 @@ export default function FNOLClaimsTable() {
 
     try {
       const username = localStorage.getItem("username") ?? "";
-      console.log(`running ${process.env.NEXT_PUBLIC_API_URL}/ins_fnol/claimslist`);
+      console.log(`running ${process.env.NEXT_PUBLIC_API_URL}/ins_fnol/claimslist/${username}`);
       const res = await fetch(
-        `${process.env.NEXT_PUBLIC_API_URL}/ins_fnol/claimslist`,
+        `${process.env.NEXT_PUBLIC_API_URL}/ins_fnol/claimslist/${username}`,
       );
       if (!res.ok) throw new Error(`HTTP ${res.status}`);
       const data = await res.json();
