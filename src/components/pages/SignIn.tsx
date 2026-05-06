1f6d0e0e5f85aacd84678685458f7a88eac734d5
Victor Omos <victor@gridlockaz.com>
2026-05-06 18:58:25 +0100

    Automated Commit

--- src/components/pages/SignIn.tsx
+++ src/components/pages/SignIn.tsx
@@ -229,6 +229,7 @@ function SignInForm() {
           setAttemptedEmail(username);
           setShowPinVerification(true);
         } else {
+          console.log("adminStatus", adminStatus);
           if (Number(process.env.NEXT_PUBLIC_INSURANCE) === 1 && adminStatus) {
             window.location.href = "/admin/ins-policy";
           } else if (Number(process.env.NEXT_PUBLIC_INSURANCE) === 1 && !adminStatus) {
