4f4e7c0a3023a06ae9e68c61f87fc7e036528769
Victor Omos <victor@gridlockaz.com>
2026-05-23 15:16:48 +0100

    Automated Commit

--- src/components/LogisticsLiveTracking.tsx
+++ src/components/LogisticsLiveTracking.tsx
@@ -750,7 +750,7 @@ export default function LogisticsRoutePlanner({
             text-md
             font-bold
             ">
-            ETA: {eta || "--"} Remaining: { distanceRemaining ? `${(Number(distanceRemaining) * 0.621371).toFixed(1)} mi` : "--" } Speed: {speed} mph
+            ETA: {eta || "--"} Remaining: { distanceRemaining ? `${(Number(distanceRemaining) * 0.621371).toFixed(1)} mi (${Number(distanceRemaining).toFixed(1)} km)` : "--" } Speed: {speed ? `${speed} mph (${(Number(speed) * 1.60934).toFixed(1)} km/h)` : "--"}
         </div>
 
       </div>
